bd26381e2f3d52fed1bb4a86cc4ae8a75fd76e0c
damifernandez <defernandez.85@gmail.com>
2020-12-08 23:36:09 -0300

    ¨mitienda¨

---
+++ tienda-m7-sportwear/src/App.jsx
@@ -0,0 +1,13 @@
+import './styles/App.css';
+import NavBar from './components/NavBar'
+
+
+function App() {
+  return (
+
+      <NavBar />
+  
+  );
+}
+
+export default App;
---
+++ tienda-m7-sportwear/src/components/NavBar.jsx
@@ -0,0 +1,19 @@
+import './NavBar.css';
+
+function NavBar() {
+    return(
+        <>
+        <h1>M7 SPORTWEAR</h1>
+        <ul>
+            <li><a href="#home">Home</a></li>
+            <li><a href="#Products">Productos</a></li>
+            <li><a href="#contact">Contacto</a></li>
+            <li><a href="#about">Nosotros</a></li>
+        </ul>
+
+        </>
+    )
+
+}
+
+export default NavBar;
