32e3187eea7b9d6cee678da79fc1bba14ecc2cfb
damifernandez <defernandez.85@gmail.com>
2020-12-22 23:01:52 -0300

    ¨mitienda¨

--- tienda-m7-sportwear/src/App.jsx
+++ tienda-m7-sportwear/src/App.jsx
@@ -1,19 +1,15 @@
-import {useState} from 'react';
+
 import NavAndWidgetCart from './components/NavAndWidgetCart';
 import FeaturedProducts from './components/FeaturedProducts';
 
 
 function App() {
-  const [nombre, setNombre] = useState('1');
-
+ 
   return (
   <>
       <NavAndWidgetCart />
       <FeaturedProducts />
-      <button onClick={() => setNombre('1')}>-</button>
-      <p>{nombre}</p>
-      <button onClick={() => setNombre({nombre} +'1')}>+</button>
-      
+
   </>
   );
 }
--- tienda-m7-sportwear/src/components/ProductCard.jsx
+++ tienda-m7-sportwear/src/components/ProductCard.jsx
@@ -2,31 +2,35 @@ import {useState} from 'react';
 import './ProductCard.css';
 
 const ProductCard = ({titulo, precio}) => {
-    const [contador,setContador] = useState(1);
+    const [contador, setContador] = useState(1);
     const restaContador = () => {
         if (contador > 1){
-            setContador(contador - 1)
+            setContador(contador - 1);
         }
     }
     const onAdd = () => {
-        alert (`Agregaste ${contador} Productos al carrito`)
+        alert (`Agregaste ${contador} Productos al carrito`);
     }
 
 
     return (
         <article className="productCard">
             <img src="https://i.pinimg.com/originals/9a/98/87/9a98878c46f0afaf0997bc8fa74d27d0.jpg" alt="Mi producto de prueba"/>
-            <div>
+            <div className="contentProduct">
                 <h3>{titulo}</h3>
                 <div className="contador">
-                    <button disable= {contador === 1 ? 'disable' : null} onClick={restaContador}> - </button>
+                    <button disabled={contador === 1 ? 'disabled' : null} onClick={restaContador}> - </button>
                     <input type="text" value={contador} readOnly />
-                    <button onclick={() => setContador (contador + 1)}>+</button>
+                    <button onClick={() => setContador (contador + 1)}> + </button>
 
                 </div>
 
                 <p>${precio}</p>
-                <button onclick={onAdd}>Agregar al Carrito</button>
+                <div className="contentBotonCarrito">
+
+                <button className="agregarAlCarrito" onclick={onAdd}>Agregar al Carrito</button>
+                </div>
+                
             </div>
         </article>
     )
